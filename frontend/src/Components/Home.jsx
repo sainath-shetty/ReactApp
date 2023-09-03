47e7fbd3a63cbe98e55621e9b136364f81b43b61
sainath-shetty <86493033+sainath-shetty@users.noreply.github.com>
2023-09-03 17:09:55 +0530

    final commit

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 const Home = () => {
   return (
     <div>
       HOME
+     <Link to='/signup'> <button>Signup</button></Link>
+     <Link to='/login'><button>Login</button></Link>
     </div>
+  
   )
 }
 
